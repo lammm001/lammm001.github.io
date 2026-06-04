05490b1d65e6c633ead44fc6c2bfc85278f1529e
V.I <vlad.ivashkin@surasystems.com>
2026-06-04 19:27:05 +0300

    docs

---
+++ docs/plugins/tmdb_proxy.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+
+    Lampa.TMDB.image = function (url) {
+      var base = Lampa.Utils.protocol() + 'image.tmdb.org/' + url;
+      return Lampa.Storage.field('proxy_tmdb') ? 'http://imagetmdb.cub.watch/' + url : base;
+    };
+
+    Lampa.TMDB.api = function (url) {
+      var base = Lampa.Utils.protocol() + 'api.themoviedb.org/3/' + url;
+      return Lampa.Storage.field('proxy_tmdb') ? 'http://apitmdb.cub.watch/3/' + url : base;
+    };
+
+    Lampa.Settings.listener.follow('open', function (e) {
+      if (e.name == 'tmdb') {
+        e.body.find('[data-parent="proxy"]').remove();
+      }
+    });
+
+})();
