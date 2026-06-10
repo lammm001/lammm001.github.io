6edad09e14db1701a5a7a25e81338ff1b17016f2
V.I <vlad.ivashkin@surasystems.com>
2026-06-10 18:37:40 +0300

    hamster fixed

--- plugins/my_xXx/xXamster.js
+++ plugins/my_xXx/xXamster.js
@@ -2,8 +2,8 @@ function xXamster(component) {
     var network = new Lampa.Reguest();
     // let proxy = ''
     // let proxy = 'https://cors.fx666.workers.dev/'
-    // let proxy = 'https://cr1.lammm.deno.net/'
-    let proxy = ''
+    let proxy = 'https://cr1.lammm.deno.net/'
+    // let proxy = ''
     const baseUrl = proxy + 'https://ru.xhamster.com';
 
     let durationMapping = {
